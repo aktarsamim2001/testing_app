678e898400c52b0970480bb808d4c543fd1d9581
aktarsamim2001 <samima@notebrains.com>
2025-12-10 11:44:48 +0530

    build issue fixed

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -42,8 +42,16 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
+        PreviousMonthButton: ({ ...props }) => (
+          <button {...props}>
+            <ChevronLeft className="h-4 w-4" />
+          </button>
+        ),
+        NextMonthButton: ({ ...props }) => (
+          <button {...props}>
+            <ChevronRight className="h-4 w-4" />
+          </button>
+        ),
       }}
       {...props}
     />
